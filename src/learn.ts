e83ab13652f82100ac2ae043056bf6c448ead5b4
EDOHWARES <edohemmanuel4real@gmail.com>
2024-06-23 13:36:39 +0100

    learning type annotations

---
+++ src/learn.ts
@@ -0,0 +1,18 @@
+// console.log('Hello Typescript World');
+
+// let word: string = 'Edmmanuel';
+// let amount: number = 20;
+// let decided: boolean = false;
+
+// console.log(word,amount, decided)
+
+
+let firstVar: string = 'this is a string';
+console.log(firstVar.toUpperCase())
+let secondVar: number = 443;
+console.log(secondVar + 2);
+let thirdVar: boolean = true
+
+if(typeof(firstVar) == 'string') {
+    console.log(!thirdVar)
+}

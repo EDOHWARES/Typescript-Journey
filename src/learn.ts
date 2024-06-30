ffa644ccbbabcec8142bff1fea2eaaeabb5152cf
EDOHWARES <edohemmanuel4real@gmail.com>
2024-06-30 13:28:26 +0100

    learing data-types of any, literals and arrays in ts

--- src/learn.ts
+++ src/learn.ts
@@ -7,12 +7,35 @@
 // console.log(word,amount, decided)
 
 
-let firstVar: string = 'this is a string';
-console.log(firstVar.toUpperCase())
-let secondVar: number = 443;
-console.log(secondVar + 2);
-let thirdVar: boolean = true
+// let firstVar: string = 'this is a string';
+// console.log(firstVar.toUpperCase())
+// let secondVar: number = 443;
+// console.log(secondVar + 2);
+// let thirdVar: boolean = true
 
-if(typeof(firstVar) == 'string') {
-    console.log(!thirdVar)
-}
+// if(typeof(firstVar) == 'string') {
+//     console.log(!thirdVar)
+// }
+
+// let requestStatus: 'pending' | 'success' | 'error';
+// requestStatus = 'pending';
+
+// const books = ['1984', 'Brave New world', 'Fahrenheit 451'];
+
+// let foundBook: string | undefined;
+
+// for (let book of books) {
+//     if (book == 'Brave New world') {
+//         foundBook = book.toUpperCase();
+//         console.log(foundBook)
+//         break;
+//     }
+// };
+
+// console.log(foundBook?.length);
+
+// let prices: number[] = [100, 200]
+// let fruit: string[] = ['apple', 'banana', 5]
+// let randomValues: [] = []
+// let names = ['peter', 'john'];
+let arr: (string | boolean)[] = ['hey', false, 'yeah', false];
